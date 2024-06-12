0f5c9cca379873be1064c89a0266458fb959672c
Jose Bizet <jlbizet90@gmail.com>
2024-06-12 12:30:06 -0300

    Adding Pojos

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String petTypeName;
+
+    public String getPetTypeName() {
+        return petTypeName;
+    }
+
+    public void setPetTypeName(String petTypeName) {
+        this.petTypeName = petTypeName;
+    }
+}
